493223b152ec6855f6df4bc08884d57e6ad18ad6
İlkay Yağız Gür <gurilkayyagiz@gmail.com>
2022-05-28 14:23:28 +0300

    Initial Commit

---
+++ Sosyopix Case Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sosyopix Case Study
+//
+//  Created by Ada Yagiz Gur on 28.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
